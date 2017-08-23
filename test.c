b3ae82bd1ddbb01eb7a083ac204697860c58611d
kilkun <kilkun117@gmail.com>
2017-08-23 15:12:38 -0400

    Why am I even doing this

---
+++ test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define MAXINT 256;
+#define HANG scanf("end");
+
+
+//This is the start
+
+void main (){
+	int maximum_integer = MAXINT;
+	printf("%d\n",maximum_integer);
+	HANG;
+}
